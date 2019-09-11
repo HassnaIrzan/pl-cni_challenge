b74430bae90c5543184efb8bca76455885d73e04
HassnaIrzan <hassna.irzan.17@ucl.ac.uk>
2019-09-11 14:20:09 +0100

    save diagnosis in groundtruth.txt

--- cni_challenge/classification.py
+++ cni_challenge/classification.py
@@ -11,16 +11,16 @@ from joblib import  load
 import numpy as np
 
 def save_file(file_name, data):
-    
+
     with open(file_name, "w") as f:
         np.savetxt(f, data.astype(int), fmt='%i', delimiter=",")
-    
+
 
 def predict_diagnosis(input_dir, ouput_dir, classifier):
-    
+
     atlas="aal"
-    
-    conn_coefs = get_classification_data(input_dir, atlas)
+
+    conn_coefs, diagnosis = get_classification_data(input_dir, atlas)
 
     classifier_fit = load(classifier)
 
@@ -28,7 +28,7 @@ def predict_diagnosis(input_dir, ouput_dir, classifier):
 
     # Output and save
     output_prediction_file_name = ouput_dir+'classification.txt'
-    
-    save_file(output_prediction_file_name, diagnosis_predict)
-    
+    output_goundtruth_file_name = ouput_dir+'goundtruth.txt'
 
+    save_file(output_prediction_file_name, diagnosis_predict)
+    save_file(output_goundtruth_file_name, diagnosis)
